77eb993b1eb1b3cbbd6b8d86662f73090481777d
Gilbert Macias <gmacias108@gmail.com>
2025-11-18 16:25:09 -0500

    Se crea el proyecto con ejemplos de operadores y funciones

---
+++ projectOpfunction/assest/js/funciones.js
@@ -0,0 +1,6 @@
+/**
+ * Funciones
+ */
+function myFunction(){
+    document.getElementById("demo4").innerHTML = "Gilbert macias";
+}
---
+++ projectOpfunction/assest/js/operadores.logicos.js
@@ -0,0 +1,10 @@
+/**
+ * Operadores logicos
+ */
+
+let x = 6;
+let y = 3;
+
+document.getElementById("demo3").innerHTML =
+    (x < 10 && y > 1) + "<br>" +
+    (x < 10 && y < 1);
